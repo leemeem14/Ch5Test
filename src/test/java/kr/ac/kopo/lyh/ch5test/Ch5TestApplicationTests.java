ae901f6ccb1209690bd3fc6b7440de9de548cf33
leemeem14 <leemeem19@naver.com>
2025-04-09 09:27:58 +0900

    Initial commit

---
+++ src/test/java/kr/ac/kopo/lyh/ch5test/Ch5TestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.lyh.ch5test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch5TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
